860982692af97687e27972951828cd46572c33fe
ruxiiiiii <ruxi749@gmail.com>
2020-08-10 17:07:17 +0300

    Added authentication and different operations you can execute while yu are logged in and restrictions that you have on the blog when you are not logged in

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+from blog import views
+
 
 urlpatterns = [
+  path('admin/', admin.site.urls),
+  #path('', include('blog.urls')),
   path('', views.post_list, name='post_list'),
   path('post/<int:pk>/', views.post_detail, name='post_detail'),
   path('post/new/', views.post_new, name='post_new'),
   path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+  path('drafts/', views.post_draft_list, name='post_draft_list'),
+  path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+  path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
